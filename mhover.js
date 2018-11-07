aa9f47b71e8e21bbb35bf7b17932e0b7f8f1bfbd
giottachou <43947889+giottachou@users.noreply.github.com>
2018-11-07 18:45:15 +0200

    Update mhover.js

--- mhover.js
+++ mhover.js
@@ -2,7 +2,7 @@ function mhover () {
  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf ("MSIE 7")!=-1) || (navigator.appVersion.indexOf ("MSIE 8")!=-1 )) {
   if (document.all)
   {
-   document.all.sound.src = "click.mp3";
+   document.all.sound.src = "Bobby_Balls.mp3";
   }
  }
  
